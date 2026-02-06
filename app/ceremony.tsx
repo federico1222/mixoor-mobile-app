22d7ffdabca8340afb0c7843c0da3dff5c860e46
federico1222 <fedeasaad1099@outlook.com.ar>
2026-02-06 13:51:53 -0300

    feat: remove reset-project script and add new app structure
    - Deleted the reset-project.js script that was used to reset the project state.
    - Created a new ceremony component in app/ceremony.tsx.
    - Added an empty index component in app/index.tsx.
    - Introduced an init component in app/init.tsx.
    - Configured Tamagui in src/config/tamagui.ts with default settings.

---
+++ app/ceremony.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "tamagui";
+
+export default function HermezCeremony() {
+  return (
+    <View flex={1}>
+      <Text>Ceremony</Text>
+    </View>
+  );
+}
---
+++ app/init.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "tamagui";
+
+export default function Admin() {
+  return (
+    <View flex={1}>
+      <Text>Init</Text>
+    </View>
+  );
+}
